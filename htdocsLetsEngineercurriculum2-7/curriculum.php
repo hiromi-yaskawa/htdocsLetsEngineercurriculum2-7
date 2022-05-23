fa6f8e6c01e57b431f125f726d45eec927efc3cb
yasukawa-hiromi <93073778+hiromi-yaskawa@users.noreply.github.com>
2022-05-24 05:29:50 +0900

    Add files via upload

---
+++ htdocsLetsEngineercurriculum2-7/curriculum.php
@@ -0,0 +1,18 @@
+<?php
+$colors= ["red"=>"赤","blue"=>"青","green"=>"緑"];
+var_dump($colors);
+?>
+
+
+
+
+
+
+</br>
+<?php
+$colors =["red"=>"赤","blue"=>"青","green"=>"緑"];
+$colors["yellow"] ="黄色";
+var_dump($colors);
+?>
+
+</br>
